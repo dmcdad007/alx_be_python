69c435d024895515808d6ea19eb0c868f00e9f6d
Dominic Solomon Agiode <d.mcdad623@gmail.com>
2025-09-27 16:30:39 +0100

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
@@ -27,3 +27,4 @@ def main():
 if __name__ == "__main__":
 
     main()
+
