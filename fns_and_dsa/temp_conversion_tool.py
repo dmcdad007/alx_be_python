e8802aaaa23fd5a01221d4d59b017e15b0dd1e46
Dominic Solomon Agiode <d.mcdad623@gmail.com>
2025-09-27 22:47:54 +0100

    Update temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,12 +1,13 @@
-def convert_to_celsius(fahrenheit):
-    FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-    """Convert Fahrenheit to Celsius using global conversion factor."""
-    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
 def convert_to_fahrenheit(celsius):
-    CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
     """Convert Celsius to Fahrenheit using global conversion factor."""
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+
+def convert_to_celsius(fahrenheit):
+    """Convert Fahrenheit to Celsius using global conversion factor."""
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def main():
     print("Temperature Conversion Tool")
@@ -29,6 +30,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
